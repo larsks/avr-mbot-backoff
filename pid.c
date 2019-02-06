dfe72337a73c9244335166ce44ace82f162751c2
Lars Kellogg-Stedman <lars@oddbit.com>
2019-02-06 18:35:20 -0500

    added pointer to upstream pid source

--- pid.c
+++ pid.c
@@ -1,84 +1,165 @@
+/**
+ * \file pid.c
+ *
+ * This is a port of Mike Matera's [FastPID][] code.  From Mike's `README`:
+ *
+ * > This PID controller is faster than alternatives for Arduino because it avoids
+ * > expensive floating point operations. The PID controller is configured with
+ * > floating point coefficients and translates them to fixed point internally. This
+ * > imposes limitations on the domain of the coefficients. Setting the I and D
+ * > terms to zero makes the controller run faster. The controller is configured to
+ * > run at a fixed frequency and calling code is responsible for running at that
+ * > frequency. The Ki and Kd parameters are scaled by the frequency to save time
+ * > during the step() operation.
+ *
+ * [fastpid]: https://github.com/mike-matera/FastPID
+ */
+
 #include <stdint.h>
+#include <stdbool.h>
 
 #include "pid.h"
-#include "millis.h"
 
-// interval is in ms
-void pid_new(PID *pid, timer_t interval,
-        float minval, float maxval,
-        float kP, float kI, float kD) {
-
-    pid->interval   = interval;
-    pid->lastupdate = 0;
-    pid->setval     = 0;
-    pid->lastval    = 0;
-    pid->error      = 0;
-    pid->lasterror  = 0;
-    pid->output     = 0;
-
-    pid_set_tuning(pid, kP, kI, kD);
-    pid_set_limits(pid, minval, maxval);
+void pid_new(PID *pid, float kp, float ki, float kd,
+        float hz, int bits, bool sign) {
+    pid_configure(pid, kp, ki, kd, hz, bits, sign);
 }
 
-void pid_set_tuning(PID *pid, float kP, float kI, float kD) {
-    float interval_in_seconds = ((float)pid->interval)/1000;
+uint32_t pid_float_to_param(PID *pid, float in) {
+  if (in > PARAM_MAX || in < 0) {
+    pid->_cfg_err = true;
+    return 0;
+  }
 
-    pid->kP = kP;
-    pid->kI = kI * interval_in_seconds;
-    pid->kD = kD / interval_in_seconds;
+  uint32_t param = in * PARAM_MULT;
+
+  if (in != 0 && param == 0) {
+    pid->_cfg_err = true;
+    return 0;
+  }
+  
+  return param;
 }
 
-void pid_set_limits(PID *pid, float minval, float maxval) {
-    pid->minval = minval;
-    pid->maxval = maxval;
-
-    if (pid->tI > maxval)
-        pid->tI = maxval;
-    else if (pid->tI < minval)
-        pid->tI = minval;
-
-    if (pid->output > maxval)
-        pid->output = maxval;
-    else if (pid->output < minval)
-        pid->output = minval;
+void pid_set_cfg_err(PID *pid) {
+  pid->_cfg_err = true;
+  pid->_p = pid->_i = pid->_d = 0;
 }
 
-void pid_set_target(PID *pid, float setval) {
-    pid->setval = setval;
+void pid_clear(PID *pid) {
+  pid->_last_sp = 0; 
+  pid->_last_out = 0;
+  pid->_sum = 0; 
+  pid->_last_err = 0;
+  pid->_cfg_err = false;
+} 
+
+bool pid_set_coefficients(PID *pid, float kp, float ki, float kd, float hz) {
+  pid->_p = pid_float_to_param(pid, kp);
+  pid->_i = pid_float_to_param(pid, ki / hz);
+  pid->_d = pid_float_to_param(pid, kd * hz);
+  return ! pid->_cfg_err;
 }
 
-float pid_update(PID *pid, float curval) {
-    float error,
-          d_error,
-          d_input, tI;
-
-    timer_t now;
-
-    now = millis();
-
-    if (now - pid->lastupdate >= pid->interval) {
-        error = (pid->setval - curval);
-   
-        pid->tI += (pid->kI * pid->error);
-        if (pid->tI > pid->maxval)
-            pid->tI = pid->maxval;
-        else if (pid->tI < pid->minval)
-            pid->tI = pid->minval;
-
-        d_error = (error - pid->lasterror);
-        d_input = (curval - pid->lastval);
-
-        pid->output = (pid->kP * error) +
-                      (pid->tI) -
-                      (pid->kD * d_input);
-        if (pid->output > pid->maxval)
-            pid->output = pid->maxval;
-        else if (pid->output < pid->minval)
-            pid->output = pid->minval;
-
-        pid->lastupdate = now;
-        pid->lastval = curval;
+bool pid_set_output(PID *pid, int bits, bool sign) {
+  // Set output bits
+  if (bits > 16 || bits < 1) {
+    pid_set_cfg_err(pid);
+  }
+  else {
+    if (bits == 16) {
+      pid->_outmax = (0xFFFFULL >> (17 - bits)) * PARAM_MULT;
     }
+    else{
+      pid->_outmax = (0xFFFFULL >> (16 - bits)) * PARAM_MULT;
+    }
+    if (sign) {
+      pid->_outmin = -((0xFFFFULL >> (17 - bits)) + 1) * PARAM_MULT;
+    }
+    else {
+      pid->_outmin = 0;
+    }
+  }
+  return ! pid->_cfg_err;
+}
 
-    return pid->output;
+bool pid_set_limits(PID *pid, int16_t min, int16_t max)
+{
+  if (min >= max) {
+    pid_set_cfg_err(pid);
+    return ! pid->_cfg_err;
+  }
+  pid->_outmin = (int64_t)(min) * PARAM_MULT;
+  pid->_outmax = (int64_t)(max) * PARAM_MULT;
+  return ! pid->_cfg_err;
+}
+
+bool pid_configure(PID *pid, float kp, float ki, float kd, float hz, int bits, bool sign) {
+  pid_clear(pid);
+  pid_set_coefficients(pid, kp, ki, kd, hz);
+  pid_set_output(pid, bits, sign);
+  return ! pid->_cfg_err; 
+}
+
+int16_t pid_step(PID *pid, int16_t sp, int16_t fb) {
+
+  // int16 + int16 = int17
+  int32_t err = (int32_t)(sp) - (int32_t)(fb);
+  int32_t P = 0, I = 0;
+  int32_t D = 0;
+
+  if (pid->_p) {
+    // uint16 * int16 = int32
+    P = (int32_t)(pid->_p) * (int32_t)(err);
+  }
+
+  if (pid->_i) {
+    // int17 * int16 = int33
+    pid->_sum += (int64_t)(err) * (int64_t)(pid->_i);
+
+    // Limit sum to 32-bit signed value so that it saturates, never overflows.
+    if (pid->_sum > INTEG_MAX)
+      pid->_sum = INTEG_MAX;
+    else if (pid->_sum < INTEG_MIN)
+      pid->_sum = INTEG_MIN;
+
+    // int32
+    I = pid->_sum;
+  }
+
+  if (pid->_d) {
+    // (int17 - int16) - (int16 - int16) = int19
+    int32_t deriv = (err - pid->_last_err) - (int32_t)(sp - pid->_last_sp);
+    pid->_last_sp = sp; 
+    pid->_last_err = err; 
+
+    // Limit the derivative to 16-bit signed value.
+    if (deriv > DERIV_MAX)
+      deriv = DERIV_MAX;
+    else if (deriv < DERIV_MIN)
+      deriv = DERIV_MIN;
+
+    // int16 * int16 = int32
+    D = (int32_t)(pid->_d) * (int32_t)(deriv);
+  }
+
+  // int32 (P) + int32 (I) + int32 (D) = int34
+  int64_t out = (int64_t)(P) + (int64_t)(I) + (int64_t)(D);
+
+  // Make the output saturate
+  if (out > pid->_outmax) 
+    out = pid->_outmax;
+  else if (out < pid->_outmin) 
+    out = pid->_outmin;
+
+  // Remove the integer scaling factor. 
+  int16_t rval = out >> PARAM_SHIFT;
+
+  // Fair rounding.
+  if (out & (0x1ULL << (PARAM_SHIFT - 1))) {
+    rval++;
+  }
+
+  pid->_last_out = rval;
+  return rval;
 }
--- pid.h
+++ pid.h
@@ -1,37 +1,56 @@
+/**
+ * \file pid.c
+ *
+ * This is a port of Mike Matera's [FastPID][] code.  From Mike's `README`:
+ *
+ * > This PID controller is faster than alternatives for Arduino because it avoids
+ * > expensive floating point operations. The PID controller is configured with
+ * > floating point coefficients and translates them to fixed point internally. This
+ * > imposes limitations on the domain of the coefficients. Setting the I and D
+ * > terms to zero makes the controller run faster. The controller is configured to
+ * > run at a fixed frequency and calling code is responsible for running at that
+ * > frequency. The Ki and Kd parameters are scaled by the frequency to save time
+ * > during the step() operation.
+ *
+ * [fastpid]: https://github.com/mike-matera/FastPID
+ */
+
 #ifndef _pid_h
 #define _pid_h
 
 #include <stdint.h>
 
-#include "millis.h"
+#define INTEG_MAX    (INT32_MAX)
+#define INTEG_MIN    (INT32_MIN)
+#define DERIV_MAX    (INT16_MAX)
+#define DERIV_MIN    (INT16_MIN)
+
+#define PARAM_SHIFT  8
+#define PARAM_BITS   16
+#define PARAM_MAX    (((0x1ULL << PARAM_BITS)-1) >> PARAM_SHIFT) 
+#define PARAM_MULT   (((0x1ULL << PARAM_BITS)) >> (PARAM_BITS - PARAM_SHIFT)) 
 
 typedef struct PID {
-    float kP,
-          kI,
-          kD;
+    uint32_t _p, _i, _d;
+    int64_t _outmax, _outmin;
+    bool _cfg_err;
 
-    float tI,
-          tD;
-
-    float setval,
-          lastval,
-          error,
-          lasterror,
-          output;
-
-    float minval,
-          maxval;
-
-    timer_t interval,
-            lastupdate;
+    int16_t _last_sp, _last_out;
+    int64_t _sum;
+    int32_t _last_err;
 } PID;
 
-void pid_new(PID *pid, timer_t interval,
-        float minval, float maxval,
-        float kP, float kI, float kD);
-void pid_set_target(PID *pid, float setval);
-void pid_set_tuning(PID *pid, float kP, float kI, float kD);
-void pid_set_limits(PID *pid, float minval, float maxval);
-float pid_update(PID *pid, float curval);
+/*
+  A fixed point PID controller with a 32-bit internal calculation pipeline.
+*/
+void pid_new(PID *pid, float kp, float ki, float kd,
+        float hz, int bits, bool sign);
+bool pid_set_coefficients(PID *pid, float kp, float ki, float kd, float hz);
+bool pid_set_output(PID *pid, int bits, bool sign);
+bool pid_set_limits(PID *pid, int16_t min, int16_t max);
+void pid_clear(PID *pid);
+bool pid_configure(PID *pid, float kp, float ki, float kd,
+        float hz, int bits, bool sign);
+int16_t pid_step(PID *pid, int16_t sp, int16_t fb);
 
 #endif // _pid_h
