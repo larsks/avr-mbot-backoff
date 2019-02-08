1118984c5f74029738441c3a691253015da08d55
Lars Kellogg-Stedman <lars@oddbit.com>
2019-02-07 19:10:12 -0500

    docs for distance.[ch]

--- distance.c
+++ distance.c
@@ -1,3 +1,10 @@
+/**
+ * \file distance.c
+ *
+ * Distance measurement using an [HC-SR04][] style sensor.
+ *
+ * [hc-sr04]: https://cdn.sparkfun.com/datasheets/Sensors/Proximity/HCSR04.pdf
+ */
 #include <stdint.h>
 #include <stdbool.h>
 
@@ -10,17 +17,38 @@
 
 #define T_MEASURE_START 100
 
+/**
+ * We want to start a new measurement roughly every 100ms.  The
+ * `TIMER2_COMPA_vect` ISR fires about once/ms, so we decrement this
+ * variable each time the ISR fires and when we reach zero start a new
+ * measurement.
+ */
 volatile uint8_t t_measure = T_MEASURE_START;
-volatile timer_t echo_start,
-         echo_end,
-         echo_duration;
+volatile timer_t
+         echo_start,    //!< micros() at start of sensor response
+         echo_end,      //!< micros() at end of sensor response
+         echo_duration; //!< duration of sensor response in ms
+
 volatile bool measure_valid = false;
 
 void measure_begin() {
+    /**
+     * TIMER2 is already being used by the `millis()`/`micros()`
+     * implementation (and TIMER0 is used to drive the PWM signal for
+     * the motors). Rather than consuming the last remaining timer, we
+     * hook up an ISR to the compare match interrupt.
+     */
     TIMSK2 |= _BV(OCIE2A);
     TIFR2 |= _BV(OCF2A);
+
     OCR2A = 128;
     
+    /**
+     * We process the return signal from the sensor using a pin change
+     * interrupt handler. The handler is triggered twice for each
+     * response: once on the rising edge of the signal, once on the
+     * falling edge.
+     */
     PCMSK1 |= _BV(PCINT9);
     PCICR |= _BV(PCIE1);
 }
@@ -36,6 +64,12 @@ timer_t measure_value() {
 }
 
 /**
+ * TIMER2 compare match interrupt handler.  This runs about 1/ms and
+ * is responsible for scheduling measurements from the ultrasonic
+ * sensor.
+ *
+ * When it is time to trigger a new measurement, we send a 10us high
+ * pulse to the sensor, then configure the data pin as an input.
  *
  * ## Timing diagram
  *
@@ -60,12 +94,17 @@ ISR(TIMER2_COMPA_vect) {
         t_measure = T_MEASURE_START;
         USDDR |= USPIN;
         USPORTREG |= USPIN;
-        _delay_us(20);
+        _delay_us(10);
         USPORTREG &= ~(USPIN);
         USDDR &= ~(USPIN);
     }
 }
 
+/**
+ * Pin change interrupt handler. This is triggered on the rising and
+ * falling edge of the response from the sensor. We record the value
+ * of `micros()` at each point, and then calculate the difference.
+ */
 ISR(PCINT1_vect) {
     if (USPINREG & USPIN) {
         echo_start = micros();
